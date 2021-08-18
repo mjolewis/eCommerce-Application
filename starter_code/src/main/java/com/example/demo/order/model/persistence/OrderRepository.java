1d94079a0ee69c30f388b681b5a5b2e17aef64d7
Michael Lewis <mjolewis@gmail.com>
2021-08-17 18:49:55 -0700

    FIX: Added repository annotations to repository interfaces

--- starter_code/src/main/java/com/example/demo/order/model/persistence/OrderRepository.java
+++ starter_code/src/main/java/com/example/demo/order/model/persistence/OrderRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.user.model.persistence.User;
 import com.example.demo.user.model.requests.UserOrder;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<UserOrder, Long> {
 	List<UserOrder> findByUser(User user);
 }
--- starter_code/src/main/java/com/example/demo/user/model/persistence/UserRepository.java
+++ starter_code/src/main/java/com/example/demo/user/model/persistence/UserRepository.java
@@ -3,7 +3,9 @@ package com.example.demo.user.model.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.user.model.persistence.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	User findByUsername(String username);
 }
